66cf64133bfae0afbcf8f3640131ccf56ba2755b
RoninLexx <lexxpsycho@yandex.ru>
2024-10-08 18:58:43 +0700

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -19,30 +19,39 @@ class House:
 
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+
 
 
     def __lt__(self, other):
         return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        return self.__eq__(other) or self.__lt__(other)
+
 
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        return not self.__le__(other)
 
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        return not self.__lt__(other)
+
 
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
-
+        return not self.__eq__(other)
 
     def __add__(self, value):
-        return House(self.name, self.number_of_floors + value)
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        return self
 
     def __iadd__(self, value):
         if isinstance(value, int):
